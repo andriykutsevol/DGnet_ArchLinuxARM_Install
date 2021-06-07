c6e05451adaf064aa7e2929b91b1f6c3f622c2f2
AndriyK <andriykutsevol@gmail.com>
2021-06-07 21:14:25 +0300

    asdf

--- ArchToPi_PostInstall.sh
+++ ArchToPi_PostInstall.sh
@@ -25,26 +25,29 @@ sed -i '/root ALL=(ALL) ALL/a dgnet ALL=(ALL) ALL' /etc/sudoers
 ##</end> Install the sudo package
 
 
-## Install the git package
-pacman -S git --noconfirm
-##</> Install the git package
-
-
 ## Install the base-devel package
 pacman -S base-devel --noconfirm
 ##</> Install the base-devel package
 
+## Install the git package
+pacman -S git --noconfirm
+##</> Install the git package
+
 doFlush
 
-pacman -S python-setuptools
+pacman -S python3 --noconfirm
+
+pacman -S python-pip --noconfirm
+
+pacman -S python-wheel --noconfirm
+
+pacman -S python-setuptools --noconfirm
 
 
-# pacman -S python-pip
-# error: failed retrieving file 'python-idna-3.1-1-any.pkg.tar.zst.sig' from mirror.osbeck.com
-
-# Therefore
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 get-pip.py
+# OR
+# curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+# python3 get-pip.py
 # python3 -m pip --version
 
+doFlush
 echo "Done"
